a80d91c7fbc476323e9d0920d0c0c6a6989e70ab
Jason Feimster <jasonfeimster@gmail.com>
2026-06-26 12:41:34 -0400

    Add business profile type

---
+++ src/types/businessProfile.ts
@@ -0,0 +1,15 @@
+export interface BusinessProfile {
+  businessName: string;
+  entityType: string;
+  formationDate: string;
+  businessLicense: boolean;
+  monthlyRevenueRange: string;
+  primaryGoal: string;
+  hasEin: boolean;
+  hasBusinessBankAccount: boolean;
+  hasBusinessAddress: boolean;
+  hasBusinessPhone: boolean;
+  hasWebsite: boolean;
+  hasDomainEmail: boolean;
+  hasDuns: boolean;
+}
